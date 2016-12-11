511e182dcf9df15cc83a1a33717ef80d3da21f71
Jeffrey Van <Jeff@jeffreys-mbp.attlocal.net>
2016-12-11 14:19:51 -0800

    added submit handler

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,6 +1,12 @@
+var amt = parseInt(document.getElementById('InitAmount'));
+var percent = parseInt(document.getElementById('Percent'));
+
 
 
 function calc_goal (init_amt, percentage) {
   if(percentage < 0 or percentage > 100)document.getElementById('goal').innerHTML = "Sorry, you can't save less than 0% or higher than 100%!";
   document.getElementById('goal').innerHTML = "In order to save " + percentage + " of your initial amount, you need to save $" + (init_amt * (percentage/100));
 }
+
+
+document.getElementById("Submit").addEventListener("submit", calc_goal(amt, percent), false);
