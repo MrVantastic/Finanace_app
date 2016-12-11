311a776110006a24234a8fd2254c866823377d3b
Jeffrey Van <Jeff@jeffreys-mbp.attlocal.net>
2016-12-11 15:14:45 -0800

    added javascript and html stuff

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,12 +1,22 @@
-var amt = parseInt(document.getElementById('InitAmount'));
-var percent = parseInt(document.getElementById('Percent'));
 
 
-
-function calc_goal (init_amt, percentage) {
-  if(percentage < 0 or percentage > 100)document.getElementById('goal').innerHTML = "Sorry, you can't save less than 0% or higher than 100%!";
-  document.getElementById('goal').innerHTML = "In order to save " + percentage + " of your initial amount, you need to save $" + (init_amt * (percentage/100));
+function calc_goal () {
+  var amt = parseInt(document.getElementById('InitAmount').value);
+  var percent = parseInt(document.getElementById('Percent').value);
+  console.log("Hello World");
+  console.log(amt);
+  console.log(percent);
+  console.log(typeof(amt));
+  console.log(typeof(percent));
+  if(percent < 0 || percent > 100){
+    document.getElementById('goal').innerHTML = "Sorry, you can't save less than 0% or higher than 100%! You also can't start out with a negative amount!";
+  }
+  if(amt < 0){
+    document.getElementById('goal').innerHTML = "You also can't start out with a negative amount!";
+  }
+  document.getElementById('goal').innerHTML = "In order to save " + percent + "% of your initial amount, you need to save $" + (amt * (percent/100));
 }
 
 
-document.getElementById("Submit").addEventListener("submit", calc_goal(amt, percent), false);
+
+document.getElementById("Submit").addEventListener("click", function() {calc_goal(); }, true);
